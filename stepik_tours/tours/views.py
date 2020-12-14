323a9432fe61306a3c6c3784fec3ce60b2f06638
Malvina Mahasim <mmahasim@ox-b5.msk.21-school.ru>
2020-12-14 19:11:13 +0300

    0

---
+++ stepik_tours/tours/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def MainView(request):
+    pass
+
+
+def DepartureView(request):
+    pass
+
+
+def TourView(request):
+    pass
